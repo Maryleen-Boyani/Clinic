ff832a770bbd8abf8a62c8ca3a7230fcf65a62c8
Maryleen Menecha <menechamaryleen@gmail.com>
2024-01-30 21:29:53 +0300

    Registered new model

--- Appointment/admin.py
+++ Appointment/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import Doctor, Patient
+from .models import Doctor, Patient, Appointments, Departments, Visits, Login
 
 # Register your models here.
 admin.site.register(Doctor)
 admin.site.register(Patient)
+admin.site.register(Appointments)
+admin.site.register(Departments)
+admin.site.register(Visits)
+admin.site.register(Login)
