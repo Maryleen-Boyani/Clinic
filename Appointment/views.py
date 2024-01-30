46c1d1ec5486690c777d2fae0c4990db4c9ed416
Maryleen Menecha <menechamaryleen@gmail.com>
2024-01-30 13:09:02 +0300

    Feat: Register serializer views

--- Appointment/views.py
+++ Appointment/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from .serializers import DoctorSerializer, PatientSerializer
+from .models import Doctor, Patient
+
 
 # Create your views here.
+class DoctorDetailsView(generics.ListCreateAPIView):
+    queryset = Doctor.objects.all()# selects all from the model Doctor
+    serializer_class = DoctorSerializer
+
+class PatientListView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Patient.objects.all()
+    serializer_class =  PatientSerializer
