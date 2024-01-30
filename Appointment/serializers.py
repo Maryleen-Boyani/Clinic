11ef462c786638edc9c09538dffbce129bdf7c47
Maryleen Menecha <menechamaryleen@gmail.com>
2024-01-30 21:56:47 +0300

    Feat: Created project serializers

--- Appointment/serializers.py
+++ Appointment/serializers.py
@@ -11,3 +11,22 @@ class PatientSerializer(serializers.ModelSerializer):
         model = Patient
         fields = ("firstname", "email", "phonenumber")
 
+class AppointmentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Appointments
+        fields = ("Patient_IDnumber", "date", "Doctor_Staffid")
+
+class DepartmentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Departments
+        fields = ("name", "dep_id")
+
+class VisitsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Visits
+        fields= ("Doctor_staffid", "Patient_idnumber", "doctors_notes", "date")
+
+class LoginSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Login
+        fields =("email", "password")
