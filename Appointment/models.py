1299ef0a5ee0962fac5da0f2d6a4ee4ec0beae30
Maryleen Menecha <menechamaryleen@gmail.com>
2024-01-30 13:05:54 +0300

    Feat: Modified the integer field

--- Appointment/models.py
+++ Appointment/models.py
@@ -4,42 +4,41 @@ from django.db import models
 # Create your models here.
 # Doctors model
 class Doctor(models.Model):
-    firstname = models.charField(max_length=50)
-    lastname = models.charField(max_length=50)
-    email = models.emaiField(max_length=50)
-    phone = models.IntegerField
-    staffid = models.IntegerField
-    department = models.charField(max_length=50)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    phonenumber = models.IntegerField()
+    staffid = models.IntegerField()
+    department = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.email
+
+
+class Patient(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    phonenumber = models.IntegerField()
+    idnumber = models.IntegerField()
 
     def __str__(self):
         return self.firstname
 
 
-class Patient(models.Model):
-    firstname = models.charField(max_length=50)
-    lastname = models.charField(max_length=50)
-    email = models.emaiField(max_length=50)
-    phone = models.IntegerField
-    idnumber = models.IntegerField
-
-
-def __str__(self):
-    return self.firstname
-
-
 class Appointments(models.Model):
     fk_idnumber = models.ForeignKey(Patient, on_delete=models.CASCADE)
     fk_staffid = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    date = models.DateField
+    date = models.DateField()
     department = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.date
+        return self.fk_staffid
 
 
 class Departments(models.Model):
     name = models.CharField(max_length=50)
-    dep_id = models.IntegerField
+    dep_id = models.IntegerField()
 
     def __str__(self):
         return self.name
@@ -48,12 +47,12 @@ class Departments(models.Model):
 class Visits(models.Model):
     fk_staffid = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     fk_idnumber = models.ForeignKey(Patient, on_delete=models.CASCADE)
-    date = models.DateField
-    doctors_notes = models.TextField
-    triage_notes = models.TextField
+    date = models.DateField()
+    doctors_notes = models.TextField()
+    triage_notes = models.TextField()
 
     def __str__(self):
-        return self.date
+        return self.fk_staffid
 
 
 class Login(models.Model):
