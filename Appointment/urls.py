e946c65595cc0ebffc4a1874c29aa19789f0fc6e
Maryleen Menecha <menechamaryleen@gmail.com>
2024-01-30 21:58:50 +0300

    Chore: Register project apps

--- Appointment/urls.py
+++ Appointment/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
-from .views import DoctorDetailsView, PatientListView
 
-urlpatterns =[
+from .views import (
+    index,
+    DoctorDetailsView,
+    PatientListView,
+    AppointmentsDetailsView,
+    DepartmentsDetailsView,
+    VisitsListView,
+    LoginListView
+)
+
+urlpatterns = [
+    path("", index, name="index"),
     path("Doctor/", DoctorDetailsView.as_view()),
     path("Patient/", PatientListView.as_view()),
+    path("Appointments/", AppointmentsDetailsView.as_view()),
+    path("Departments/", DepartmentsDetailsView.as_view()),
+    path("Visits/", VisitsListView.as_view()),
+    path("Login/", LoginListView.as_view())
 ]
